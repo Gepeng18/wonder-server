f02493ee02722e15fd0c27442f0de9b85f6adc60
whz <whz@live.com>
2024-02-27 10:54:18 +0800

    [fix] 跨域配置修改

--- wonder-admin/src/main/java/com/gitee/whzzone/admin/common/satoken/SaTokenConfigure.java
+++ wonder-admin/src/main/java/com/gitee/whzzone/admin/common/satoken/SaTokenConfigure.java
@@ -3,6 +3,7 @@ package com.gitee.whzzone.admin.common.satoken;
 import cn.dev33.satoken.config.SaTokenConfig;
 import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.filter.SaServletFilter;
+import cn.dev33.satoken.router.SaHttpMethod;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.json.JSONUtil;
@@ -83,9 +84,17 @@ public class SaTokenConfigure {
                             .setHeader("X-XSS-Protection", "1; mode=block")
                             // 禁用浏览器内容嗅探
                             .setHeader("X-Content-Type-Options", "nosniff")
-                    ;
-                })
-
-                ;
+                            // ---------- 设置跨域响应头 ----------
+                            // 允许指定域访问跨域资源
+                            .setHeader("Access-Control-Allow-Origin", "*")
+                            // 允许所有请求方式
+                            .setHeader("Access-Control-Allow-Methods", "*")
+                            // 允许的header参数
+                            .setHeader("Access-Control-Allow-Headers", "*")
+                            // 有效时间
+                            .setHeader("Access-Control-Max-Age", "3600")
+                    ;// 如果是预检请求，则立即返回到前端
+                    SaRouter.match(SaHttpMethod.OPTIONS).back();
+                });
     }
 }
